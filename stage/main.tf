43fe1cd691eb814eccd37fb27cba4786b13145c0
Administrator <gitlab_admin_3d99cc@example.com>
2024-08-07 06:42:07 +0000

    Add new file

---
+++ stage/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project     = "deloitte-team2"
+  zone        = "us-central1-c"
+  region      = "us-central1"
+}
+
+resource "google_container_cluster" "gke-cluster" {
+  name               = "mehar-gke-cluster"
+  network            = "default1"
+  location             = "us-central1-c"
+  initial_node_count = 1
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "extra-pool" {
+  name               = "extra-node-pool"
+  location             = "us-central1-c"
+  cluster            = google_container_cluster.gke-cluster.name
+  initial_node_count = 2
+}
